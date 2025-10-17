5d50173c1f2129a4fce5f7b75c91186af2295832
gitanjali <sgitanjali3112@gmail.com>
2025-10-16 20:16:10 -0500

    Run NEO at specified RPM and display to shuffleboard

--- src/main/java/team/gif/robot/commands/SetVelocitySparkMax.java
+++ src/main/java/team/gif/robot/commands/SetVelocitySparkMax.java
@@ -4,11 +4,11 @@ import edu.wpi.first.wpilibj2.command.Command;
 import team.gif.robot.Constants;
 import team.gif.robot.Robot;
 
-public class TalonMotorsSpinBackward extends Command {
+public class SetVelocitySparkMax extends Command {
 
-    public TalonMotorsSpinBackward() {
+    public SetVelocitySparkMax() {
         super();
-        addRequirements(Robot.talon); // uncomment
+        addRequirements(Robot.sparkMax);
     }
 
     // Called when the command is initially scheduled.
@@ -18,7 +18,8 @@ public class TalonMotorsSpinBackward extends Command {
     // Called every time the scheduler runs (~20ms) while the command is scheduled
     @Override
     public void execute() {
-        Robot.talon.turnMotor(-Constants.TALON_MOTOR_PERCENT);
+        Robot.sparkMax.setRPM(Constants.SPARKMAX_MOTOR_RPM);
+
     }
 
     // Return true when the command should end, false if it should continue. Runs every ~20ms.
@@ -30,6 +31,6 @@ public class TalonMotorsSpinBackward extends Command {
     // Called when the command ends or is interrupted.
     @Override
     public void end(boolean interrupted) {
-        Robot.talon.turnMotor(0);
+        Robot.sparkMax.setRPM(0);
     }
 }
--- src/main/java/team/gif/robot/subsystems/SparkMaxMotorControl.java
+++ src/main/java/team/gif/robot/subsystems/SparkMaxMotorControl.java
@@ -4,29 +4,44 @@
 
 package team.gif.robot.subsystems;
 
+import com.revrobotics.RelativeEncoder;
 import com.revrobotics.spark.SparkBase;
+import com.revrobotics.spark.SparkClosedLoopController;
 import com.revrobotics.spark.SparkLowLevel;
 import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.config.SparkBaseConfig;
 import com.revrobotics.spark.config.SparkMaxConfig;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import team.gif.robot.Constants;
 import team.gif.robot.RobotMap;
 
 public class SparkMaxMotorControl extends SubsystemBase {
 
     private SparkMax sparkMax;
+    private SparkClosedLoopController sparkPID;
+    private RelativeEncoder neoEncoder;
 
     public SparkMaxMotorControl() {
         sparkMax = new SparkMax(RobotMap.SPARKMAX_ID, SparkLowLevel.MotorType.kBrushless);
 
         SparkMaxConfig config = new SparkMaxConfig();
+        sparkPID = sparkMax.getClosedLoopController();
+        neoEncoder = sparkMax.getEncoder();
         config.idleMode(SparkBaseConfig.IdleMode.kBrake);
-
+        config.closedLoop.pid(Constants.SPARK_MOTOR_P,Constants.SPARK_MOTOR_I,0.0);
         sparkMax.configure(config, SparkBase.ResetMode.kResetSafeParameters, SparkBase.PersistMode.kPersistParameters);
     }
 
     public void setVoltage(double voltage) {
         sparkMax.setVoltage(voltage);
+    }
+
+    public void setRPM(double RPM){
+        sparkPID.setReference(RPM, SparkBase.ControlType.kVelocity);
+    }
+
+    public double getRPM(){
+        return neoEncoder.getVelocity();
     }
 
 
